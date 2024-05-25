5d2f395a40291d7a0683491b4bfe5632a1f7086d
joonhyung <asy8010@gmail.com>
2024-05-25 14:28:02 +0900

    [feat] 테스팅 프레임워크 추가(vitest, react-testing-library)

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,4 +6,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   cacheDir: "./.vite",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+  },
 });
